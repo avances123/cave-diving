3d25704af853215c33f7dc17dad81d2610853a6c
Fabio Rueda <avances123@gmail.com>
2023-12-11 23:44:01 +0100

    disclaimer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,12 @@ export class AppComponent {
     { title: 'Calcular Gas', url: 'simple/sugerir', icon: 'color-wand' },
   ];
   public trabajoPages = [
-    { title: 'Calcular tiempo disponible', url: 'trabajo/penetracion', icon: 'repeat' },
-    { title: 'Calcular Gas', url: 'trabajo/sugerir', icon: 'color-wand' },
+    // { title: 'Calcular tiempo disponible', url: 'trabajo/penetracion', icon: 'repeat' },
+    // { title: 'Calcular Gas', url: 'trabajo/sugerir', icon: 'color-wand' },
   ];
   
   public personalPages = [
-    { title: 'Blender', url: 'blender', icon: 'sync' },
+    // { title: 'Blender', url: 'blender', icon: 'sync' },
     { title: 'Datos Personales', url: 'settings', icon: 'settings' },
   ];
   constructor() {}
